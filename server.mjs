4cfbfcc35219148e5a1ec937e8751a8aafd464ab
forondalouie <forondalouie@gmail.com>
2023-11-12 09:54:17 +0800

    added server, by running yarn server:start

---
+++ server.mjs
@@ -0,0 +1,35 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import { attachWebsocketServer } from "@vlcn.io/ws-server";
+import * as http from "http";
+
+const PORT = parseInt(process.env.PORT || "8083");
+
+const app = express();
+const server = http.createServer(app);
+
+ViteExpress.config({
+  mode: "production",
+  inlineViteConfig: {
+    // base: "/admin",
+    build: { outDir: "out" }
+  }
+});
+
+const wsConfig = {
+  dbFolder: "./dbs",
+  schemaFolder: "./src/schemas",
+  pathPattern: /\/sync/,
+};
+
+attachWebsocketServer(server, wsConfig);
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+server.listen(PORT, () =>
+  console.log("info", `listening on http://localhost:${PORT}!`)
+);
+
+ViteExpress.bind(app, server);
